4bcfe2f604d232dc53ad5433be09a12474885bee
AlphaRunic <peenreee@gmail.com>
2021-05-23 21:03:26 +0100

    Fix .gitignore

--- out/Events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { log, error } = console;
-const Events = new Map([
-    ["ready", () => log("Ripple is now online.")],
-    ["error", (err) => error(err)]
-]);
-exports.default = Events;
-//# sourceMappingURL=Events.js.map
