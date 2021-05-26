62cd097b45b2d3d689aa80c391a8ced9ec587ef7
AlphaRunic <peenreee@gmail.com>
2021-05-26 06:44:08 +0100

    Add Lyrics command

---
+++ src/Commands/Info/Lyrics.ts
@@ -0,0 +1,39 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import { Arg } from "../../Ripple/Util";
+import { requestLyricsFor, requestTitleFor, requestIconFor, requestAuthorFor } from "solenolyrics";
+import RippleClient from "../../Ripple/Client";
+
+export default class extends Command<RippleClient> {
+    public constructor() {
+        const name = "lyrics";
+        super(name, {
+            aliases: [name, "songlyrics"],
+            description: {
+                content: "Returns the lyrics to a song.",
+                usage: '<"song">' 
+            },
+            args: [ Arg("song", "string") ]
+        });
+    }
+
+    public async exec(msg: Message, { song }: { song: string }) {
+        return msg.reply("Fetching lyrics, this may take a while...")
+            .then(async sent => {
+                return {
+                    sentMsg: sent,
+                    lyricsMsg: await msg.reply(
+                        this.client.Embed()
+                            .setTitle(await requestTitleFor(song))
+                            .setDescription((await requestLyricsFor(song)).slice(0, 2042))
+                            .setAuthor(await requestAuthorFor(song))
+                            .setThumbnail(await requestIconFor(song))
+                    )
+                }
+            })
+            .then(async ({ sentMsg, lyricsMsg }: { sentMsg: Message, lyricsMsg: Message }) => {
+                await sentMsg.delete();
+                return lyricsMsg;
+            });
+    }
+}
