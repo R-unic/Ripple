4ffc57b1a62173d5b09d54d91cda866a5fafb2e1
AlphaRunic <peenreee@gmail.com>
2021-05-28 04:50:26 +0100

    Add limit to Purge command

--- src/Commands/Moderation/Purge.ts
+++ src/Commands/Moderation/Purge.ts
@@ -1,51 +1,40 @@
-import { error } from "console";
 import { Command } from "discord-akairo";
 import { Message, TextChannel } from "discord.js";
+import { Arg, Clamp } from "../../Ripple/Util";
 import Ripple from "../../Ripple/Client";
 
-export default class extends Command {
+export default class extends Command<Ripple> {
     public constructor() {
         const name = "purge";
         super(name, {
-            aliases: [name, "bulkdel", "bulkdelete", "nuke", "clear"],
+            aliases: [name, "bulkdel", "bulkdelete", "nuke", "clear", "prune"],
             userPermissions: "MANAGE_MESSAGES",
             clientPermissions: "MANAGE_MESSAGES",
-            cooldown: 5,
+            cooldown: 10,
             description: {
                 content: "Deletes a number of messages at once.",
                 usage: "<numberOfMessages>"
             },
-            args: [
-                {
-                    id: "messagesToRemove",
-                    type: "number",
-                    default: 0
-                }
-            ],
+            args: [ Arg("messagesToRemove", "number") ]
         });
     }
 
-    public async exec(msg: Message, { messagesToRemove }: { messagesToRemove: number }) {
-        const client = this.client as Ripple;
-        
-        if (!messagesToRemove || messagesToRemove === 0)
-            return client.Logger.MissingArgError(msg, "messagesToRemove");
+    public async exec(msg: Message, { messagesToRemove }: { messagesToRemove: number }) {        
+        if (!messagesToRemove)
+            return this.client.Logger.MissingArgError(msg, "messagesToRemove");
 
         const channel = msg.channel as TextChannel
         if (!channel)
             return msg.reply("Invalid channel!");
 
         messagesToRemove++;
-        channel
-            .messages.fetch({
-                limit: messagesToRemove
-            })
-            .then(messages => {
-                channel.bulkDelete(messages);
-                channel
-                    .send(`Bulk delete successful. Total messages removed: ${messagesToRemove}`)
-                    .then(sent => sent.delete({ timeout: client.Seconds(10) }));
-            })
-            .catch(error);
+        return channel.messages.fetch({
+            limit: Clamp(messagesToRemove, 1, 100)
+        }).then(messages => {
+            return channel
+                .bulkDelete(messages)
+                .then(() => channel.send(`Bulk delete successful. Total messages removed: ${messages.size}`))
+                .then(sent => sent.delete({ timeout: this.client.Seconds(10) }));
+        });
     }
 }
