b628bf894819ee3e0a01c8fc91db610b101b4e66
AlphaRunic <peenreee@gmail.com>
2021-05-28 23:08:05 +0100

    Add DirectMessage command

---
+++ src/Commands/Utility/DirectMessage.ts
@@ -0,0 +1,40 @@
+import { Command } from "discord-akairo";
+import { GuildMember, Message } from "discord.js";
+import { Arg } from "../../Ripple/Util";
+import Ripple from "../../Ripple/Client";
+
+export default class extends Command<Ripple> {
+    public constructor() {
+        const name = "directmessage";
+        super(name, {
+            aliases: [name, "directmsg", "privmsg", "pm", "dm"],
+            userPermissions: "MANAGE_GUILD",
+            cooldown: 10,
+            description: {
+                content: "Sends a DM to a member as Ripple.",
+                usage: '<@member> <"content">'
+            },
+            args: [
+                Arg("member", "member"),
+                Arg("content", "string")
+            ],
+        });
+    }
+
+    public async exec(msg: Message, { member, content }: { member: GuildMember, content: string }) {
+        if (!member)
+            return this.client.Logger.MissingArgError(msg, "member");
+
+        if (member.user.bot)
+            return this.client.Logger.InvalidArgError(msg, "User cannot be a bot!");
+
+        if (!content)
+            return this.client.Logger.MissingArgError(msg, "content");
+
+        return member.send(content)
+            .then(() => msg.reply(
+                this.client.Success()
+                    .setDescription(`Successfully sent your DM to ${member}.`)
+            ));
+    }
+}
