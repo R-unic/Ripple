a8502b6cfaa6bce4b014062469f4047de43dfdc1
AlphaRunic <peenreee@gmail.com>
2021-06-01 18:30:15 +0100

    Update Package.ts

--- src/Ripple/Package.ts
+++ src/Ripple/Package.ts
@@ -1,4 +1,14 @@
 export interface Package {
     name: string;
     version: string;
+    description: string;
+    main: string;
+    author: string;
+    license: string;
+    homepage: string;
+    keywords: string[];
+    scripts: {
+        start: string;
+        test: string;
+    };
 }
