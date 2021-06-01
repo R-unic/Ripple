b458616719b7a10e8102c79657adb36863026a96
AlphaRunic <peenreee@gmail.com>
2021-06-01 07:50:01 +0100

    Add InfractionManager

---
+++ src/Ripple/Components/DataManagement/Managers/InfractionManager.ts
@@ -0,0 +1,46 @@
+import { GuildMember } from "discord.js";
+import { GuildMemberDataManager } from "../Base/GuildMemberDataManager";
+import Ripple from "../../../Client";
+
+export interface Infraction {
+    Issuer: GuildMember,
+    Recipient: GuildMember,
+    Reason: string,
+    Timestamp: Date
+}
+
+export class InfractionManager implements GuildMemberDataManager<Infraction[]> {
+    public Tag = "infractions";
+    public MaxLevel = 100;
+    public MaxPrestige = 25;
+
+    public constructor(
+        public readonly Client: Ripple
+    ) {}
+
+    public async Find(user: GuildMember, index: number): Promise<Infraction> {
+        const infractions: Infraction[] = await this.Get(user);
+        return infractions[index];
+    }
+
+    public async Remove(user: GuildMember, infraction: Infraction): Promise<boolean> {
+        const infractions: Infraction[] = await this.Get(user);
+        const idx = infractions.indexOf(infraction);
+        infractions.splice(idx - 1, 1);
+        return this.Set(user, infractions);
+    }
+
+    public async Add(user: GuildMember, infraction: Infraction): Promise<boolean> {
+        const infractions: Infraction[] = await this.Get(user);
+        infractions.push(infraction);
+        return this.Set(user, infractions);
+    }
+
+    public async Get(user: GuildMember, defaultValue?: Infraction[]): Promise<Infraction[]> {
+        return this.Client.Get(user, this.Tag, defaultValue?? [], user.id);
+    }
+
+    public async Set(user: GuildMember, value: Infraction[]): Promise<boolean> {
+        return this.Client.Set(user, this.Tag, value, user.id);
+    }
+}
