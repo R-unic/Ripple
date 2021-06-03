ff29a686865c94186c2bef816f81f66ee82da4d1
AlphaRunic <peenreee@gmail.com>
2021-06-03 07:45:45 +0100

    Add Memory Usage field to About embed

--- src/Ripple/Commands/Ripple/About.ts
+++ src/Ripple/Commands/Ripple/About.ts
@@ -3,6 +3,18 @@ import { version as discordJSVersion, Message } from "discord.js";
 import { Hyperlink, StripISO, User } from "../../Util";
 import Ripple from "../../Client";
 
+function formatBytes(bytes: number, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
 export default class extends Command<Ripple> {
     public constructor() {
         const name = "about";
@@ -12,7 +24,7 @@ export default class extends Command<Ripple> {
         });
     }
 
-    public async exec(msg: Message) {
+    public async exec(msg: Message) {        
         return msg.reply(
             this.client.Embed("Ripple")
                 .setDescription(`
@@ -28,6 +40,7 @@ export default class extends Command<Ripple> {
                 .addField("Library", `Discord.js v${discordJSVersion}`, true)
                 .addField("Language", "TypeScript 4.2.4", true)
                 .addField("Compiled Environment", `Node ${process.version}`, true)
+                .addField("Memory Usage", formatBytes(process.memoryUsage().heapTotal), true)
         );
     }
 }
