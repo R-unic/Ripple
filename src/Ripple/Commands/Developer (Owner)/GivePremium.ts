1caac275faa0f3ef1d7de8c92b68691927dafd50
AlphaRunic <peenreee@gmail.com>
2021-06-05 19:30:04 +0100

    Add GivePremium dev command rename Developer category to Developer (Owner)

---
+++ src/Ripple/Commands/Developer (Owner)/GivePremium.ts
@@ -0,0 +1,34 @@
+import { Command } from "discord-akairo";
+import { GuildMember, Message } from "discord.js";
+import { Arg } from "../../Util";
+import Ripple from "../../Client";
+
+export default class extends Command<Ripple> {
+    public constructor() {
+        const name = "givepremium";
+        super(name, {
+            aliases: [name, "awardpremium", "giveripplepremium", "awardripplepremium"],
+            ownerOnly: true,
+            description: {
+                content: "Gives a user Ripple Premium for free.",
+                usage: "<@user?>"
+            },
+            args: [ Arg("member", "member", msg => msg.member) ]
+        });
+    }
+
+    public async exec(msg: Message, { member }: { member: GuildMember }) {
+        if (!member)
+            return this.client.Logger.MissingArgError(msg, "user");
+
+        const user = member.user;
+        const hasPremium = await this.client.Premium.Get(user);
+        if (hasPremium)
+            return this.client.Logger.InvalidArgError(msg, "User already has Premium.");
+
+        return this.client.Premium.Set(user, true)
+            .then(() => msg.reply(
+                this.client.Success(`Successfully gave ${member} Ripple Premium.`)
+            ));
+    }
+}
